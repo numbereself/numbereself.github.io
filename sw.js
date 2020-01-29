ee22118756f8fde963c1a81165f62d8bc7c2b9eb
Eike-Albert Erdmann <eike.erdmann@rwth-aachen.de>
2020-01-29 18:32:35 +0100

    Change cache strat to download to cache!

--- sw.js
+++ sw.js
@@ -1,37 +1,22 @@
-let cache_name = 'unitext_v1'
+let cache_name = 'unitext_v2'
 
-let urls_to_cache = [
-    '',
-    '/',
-    '/index.html',
-    '/images/icon48.png',
-    '/build/Desktop.data.unityweb',
-    '/build/Desktop.json',
-    '/build/Desktop.wasm.code.unityweb',
-    '/build/Desktop.wasm.framework.unityweb',
-    '/build/UnityLoader.js'
-
-]
 self.addEventListener('install', (e) => {
-    console.log("NEW Service installing");
-    self.skipWaiting();
-    e.waitUntil(caches.open(cache_name).then((cache) => {
-        return cache.addAll(urls_to_cache);
-    }) )
+    console.log("Service install");
 });
 
 self.addEventListener('activate', (e) => {
-    console.log("NEW Service activate");
+    console.log("Service activate");
 });
 
-self.addEventListener('fetch', (e) => {
-    console.log("Fetching " + e.request.url)
-    e.respondWith(caches.match(e.request).then((response) => {
-     if(response)
-     {
-        return response
-     }
-     else
-      return fetch(e.request)
-    }) )
- });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        } || response);
+      });
+    })
+  );
+});
