3c80671c3ec7a46270e4261105d8fbdf450648c7
Eike-Albert Erdmann <eike.erdmann@rwth-aachen.de>
2020-01-29 14:00:10 +0100

    Indentation here too!

--- sw.js
+++ sw.js
@@ -3,11 +3,11 @@ let cache_name = 'mysite-v1'
 let urls_to_cache = [
     '',
     '/images/icon48.png',
-    '/Build/Desktop.data.unityweb',
-    '/Build/Desktop.json',
-    '/Build/Desktop.wasm.code.unityweb',
-    '/Build/Desktop.wasm.framework.unityweb',
-    '/Build/UnityLoader.js'
+    '/build/Desktop.data.unityweb',
+    '/build/Desktop.json',
+    '/build/Desktop.wasm.code.unityweb',
+    '/build/Desktop.wasm.framework.unityweb',
+    '/build/UnityLoader.js'
 
 ]
 self.addEventListener('install', (e) => {
