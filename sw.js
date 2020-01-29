60147a8dc660c68e6d7c73d5dcf526410bc2a58d
Eike-Albert Erdmann <eike.erdmann@rwth-aachen.de>
2020-01-29 14:33:43 +0100

    no work no idea no help !

--- sw.js
+++ sw.js
@@ -11,13 +11,14 @@ let urls_to_cache = [
 
 ]
 self.addEventListener('install', (e) => {
+  alert("install event");
     e.waitUntil(caches.open(cache_name).then((cache) => {
         return cache.addAll(urls_to_cache);
     }) )
 })
 
 self.addEventListener('fetch', (e) => {
-    alert(e.request.url);
+    alert(e.request.url + "yoyo");
     e.respondWith(caches.match(e.request).then((response) => {
      if(response)
      {
