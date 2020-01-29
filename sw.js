85860938623c6942b687710f5b4e969b101e7baa
Eike-Albert Erdmann <eike.erdmann@rwth-aachen.de>
2020-01-29 16:12:58 +0100

    Add caching back in!

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
-let cache_name = 'mysite-v1'
+let cache_name = 'unitext_v1'
 
 let urls_to_cache = [
-    '',
     '/',
-    '/index.html',
-    '/images/icon48.png',
-    '/build/Desktop.data.unityweb',
-    '/build/Desktop.json',
-    '/build/Desktop.wasm.code.unityweb',
-    '/build/Desktop.wasm.framework.unityweb',
-    '/build/UnityLoader.js'
-
+    '/images/icon48.png'
 ]
 self.addEventListener('install', (e) => {
     console.log("Service installing");
+    self.skipWaiting();
+    e.waitUntil(caches.open(cache_name).then((cache) => {
+        return cache.addAll(urls_to_cache);
+    }) )
 });
 
 self.addEventListener('activate', (e) => {
     console.log("Service activate");
 });
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => {
+     if(response)
+     {
+        return response
+     }
+     else
+      return fetch(e.request)
+    }) )
+ });
