679d6e5e7bf27aaeade19e5352ada0d83f7a54b6
Eike-Albert Erdmann <eike.erdmann@rwth-aachen.de>
2020-01-29 15:43:14 +0100

    Make it all go and instead build from scratch this will never work!

--- sw.js
+++ sw.js
@@ -13,20 +13,9 @@ let urls_to_cache = [
 
 ]
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(cache_name).then((cache) => {
-        return cache.addAll(urls_to_cache);
-    }) )
-})
+    console.log("Service installing");
+});
 
-self.addEventListener('fetch', (e) => {
-  error_bad();
-    e.respondWith(caches.match(e.request).then((response) => {
-     if(response)
-     {
-        return response
-     }
-      
-     else
-      return fetch(e.request)
-    }) )
-   })
+self.addEventListener('activate', (e) => {
+    console.log("Service activate");
+});
